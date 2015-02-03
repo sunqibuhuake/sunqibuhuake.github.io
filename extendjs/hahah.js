40a3e796c0df9bf6110ffb93ca2f9ee372896e9f
sunqi <12824031@qq.com>
2015-02-03 13:23:24 +0800

    delete

--- extendjs/hahah.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by sunqi on 15-1-30.
- */
-var Paction=true;
-function pause(){
-    Paction=!Paction;
-    window.frames['fuck'].action=Paction;
-    console.log(window.frames['fuck'].action);
-}
