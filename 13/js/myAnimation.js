d7fa494fe200f48d4c615764493f602fee158159
sunqi <12824031@qq.com>
2015-03-14 03:13:10 +0800

    touch

--- 13/js/myAnimation.js
+++ 13/js/myAnimation.js
@@ -1,6 +1,13 @@
 /**
  * Created by sunqi on 15-3-10.
  */
+;(function(){
+    var isTouch = ('ontouchstart' in document.documentElement) ? 'touchstart' : 'click', _on = $.fn.on;
+    $.fn.on = function(){
+        arguments[0] = (arguments[0] === 'click') ? isTouch: arguments[0];
+        return _on.apply(this, arguments);
+    };
+})();
 function xianshi(s){
     $(s).css('visibility','visible');
 }
